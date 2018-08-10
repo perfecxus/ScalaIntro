8a04db31d35e0de76c392ea5a12673119a38af4a
banerjee <mr.sinchan.banerjee@gmail.com>
2018-08-10 09:27:01 +0530

    Initial commit

---
+++ src/main/scala/com/sample/MyMain.scala
@@ -0,0 +1,18 @@
+package com.sample
+
+/**
+  * Created by sinchan on 10/08/18.
+  */
+object MyMain { //class -- class MyMain, object MyMain <---> Classes and object
+
+  def main(args: Array[String]): Unit = { //main method -> entry point for scala app . type signature - (name,the argument list with datatype, and return type)
+
+    println(" Hello World! ")
+
+  }
+
+
+  /*method1() {
+    method2()
+  }*/
+}
