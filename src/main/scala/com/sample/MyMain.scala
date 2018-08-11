4d3c836d56478a68a14e652412e54ceb0e29eff7
banerjee <mr.sinchan.banerjee@gmail.com>
2018-08-11 08:48:03 +0530

    Lecture2 changes

--- src/main/scala/com/sample/MyMain.scala
+++ src/main/scala/com/sample/MyMain.scala
@@ -9,7 +9,46 @@ object MyMain { //class -- class MyMain, object MyMain <---> Classes and object
 
     println(" Hello World! ")
 
+    var i = 1//var i = 1  -- type infered but scala is strongly typed, declare vs define
+     i = i + 1
+    //i = "abc"  //assigning i with a string value , I cannot change a variables data type dynamically, but I can cast a variable to another dta type
+    var t = i.toString //javascript --> i=1  i="abc"
+
+    val result = if(args.length == 0){
+      "No arguments passed"
+    }
+    else if(args.length == 1){
+      "one argument passed"
+    }
+    else{
+      "more than 1 argument passed"
+    }
+
+    val result3 = args.length match {
+      case 0 => "No arguments passed"
+      case 1 => "one argument passed"
+      case _ => "more than 1 argument passed"
+    }
+
+    println(s"result3 = ${result3}")
+
+    println(s"result is ${result} and i = ${i} and dynamically incremented i  = ${i+1}")
+
+
+    val arg2 = args(1) //0 indexed
+
+    val salutation = arg2 match {
+      case "hello" => "hello guys"
+      case "hi" => "hi all"
+      case "welcome" => "Welcome to the world of Scala"
+      //case 1 =>"abc"
+      case _ => "Default salutation"
+    }
+
+    println(salutation)
+
   }
+
 
 
   /*method1() {
